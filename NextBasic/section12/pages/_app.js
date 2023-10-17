b64e9c908cb20c9fc2462dd45d295e9935249146
김동민 <anwjr41@naver.com>
2023-10-18 00:51:41 +0900

    13섹션 완

--- NextBasic/section12/pages/_app.js
+++ NextBasic/section12/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(Component.Layout);
+
+  const EmptyLayout = ({ children }) => <>{children}</>;
+  const SubLayout = Component.Layout || EmptyLayout;
+  return (
+    <Layout>
+      <SubLayout>
+        <Component {...pageProps} />
+      </SubLayout>
+    </Layout>
+  );
 }
