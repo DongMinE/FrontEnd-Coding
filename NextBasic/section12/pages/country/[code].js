deb50b2ff387ad90da7c17f65b235973e03bb617
김동민 <anwjr41@naver.com>
2023-10-24 03:27:54 +0900

    15섹션 완

--- NextBasic/section12/pages/country/[code].js
+++ NextBasic/section12/pages/country/[code].js
@@ -4,8 +4,18 @@ import { useRouter } from "next/router";
 
 export default function Country({ country }) {
   const router = useRouter();
-  const { code } = router.query;
 
+  // fallback: true일 때 일단 빨리 보여줄 페이지
+  if (router.isFallback) {
+    return <div>LOADING....</div>;
+  }
+
+  // fallback 후에도 없는 페이지
+  if (!country) {
+    return <div>존재하지 않는 국가입니다.</div>;
+  }
+
+  // fallback 하고 난 뒤 진짜 페이지
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -15,7 +25,25 @@ export default function Country({ country }) {
 
 Country.Layout = SubLayout;
 
-export const getServerSideProps = async (context) => {
+/*
+SSG페이지를 생성할 때 동적으로 경로를 할당한다면 어떤 경로의 페이지를 만들어 놓을지 명시를해야함
+getStaticPaths를 통해서 경로를 명시하고 fallback를 통해 다른 경로는 관리
+
+fallback: false => 다른 경로 차단
+fallback: "blocking" => 실시간으로 새 경로 생성하고 저장
+fallback: true => 데이터가 없는 상태 페이지 줬다가 정보 저장하고 다시 보여줌
+
+*/
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { code: "ABW" } }, { params: { code: "KOR" } }],
+    fallback: true,
+  };
+};
+
+//SSG 페이지 생성하기
+export const getStaticProps = async (context) => {
   const { code } = context.params;
   let country = null;
 
--- NextBasic/section12/pages/index.js
+++ NextBasic/section12/pages/index.js
@@ -1,8 +1,6 @@
 import { fetchCountries } from "@/api";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import Country from "./country/[code]";
 
+// 원래 CSR 형식 동일
 export default function Home({ conturies }) {
   return (
     <div>
@@ -13,10 +11,27 @@ export default function Home({ conturies }) {
   );
 }
 
-export const getServerSideProps = async () => {
+/* 
+next에서 정한 함수 명 규칙
+getServerSideProps - SSR로 렌더링함 => 서버에서만 실행되므로 next의 콘솔은 브라우저에 안보임
+getStaticProps - SSG로 렌더링 함 => 처음 빌드될 때 한번만 호출함, 더이상 안바뀜
+*/
+
+/* next에서 정한 build 파일 보기
+흰 동그라미 - SSG
+빈 동그라미 - 기본 SSG
+람다 기호 - SSR
+*/
+
+// SSR 적용
+// export const getServerSideProps = async () => {
+
+// SSG 적용 - 빌드 할 때만 만들고 더이상 생성 X
+export const getStaticProps = async () => {
   // ssr을 위해 서버측에서 페이제 컴포넌트에게 전달할 데이터를 설정하는 함수
 
   const conturies = await fetchCountries();
+  console.log("컨츄리 데이터 불러옴");
   return {
     props: {
       conturies,
