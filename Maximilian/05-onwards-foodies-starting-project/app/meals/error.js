deb80fcd4e4a5ac7a3515a14b3246276640ae26a
김동민 <anwjr41@naver.com>
2024-05-22 03:56:35 +0900

    05 22 next - error, not found 추가

---
+++ Maximilian/05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>에러가 발생했습니다!</h1>
+      <p>다시 시도해주세요!</p>
+    </main>
+  );
+}
---
+++ Maximilian/05-onwards-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>죄송합니다. 요청하신 메뉴를 불러올 수 없습니다.</p>
+    </main>
+  );
+}
