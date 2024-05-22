b1eb784d56b4aac6693862acb11ffa0632b07029
김동민 <anwjr41@naver.com>
2024-05-23 03:42:40 +0900

    0523 공유글 작성 반영

--- Maximilian/05-onwards-foodies-starting-project/app/meals/page.js
+++ Maximilian/05-onwards-foodies-starting-project/app/meals/page.js
@@ -5,13 +5,12 @@ import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-async function Meals() {
-  const meals = await getMeals();
-
-  return <MealsGrid meals={meals} />;
-}
-
 export default function MealsPage() {
+  async function Meals() {
+    const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />;
+  }
   return (
     <>
       <header className={classes.header}>
---
+++ Maximilian/05-onwards-foodies-starting-project/components/meals/image-picker.js
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>선택된 이미지가 없습니다.</p>}
+          {pickedImage && <Image src={pickedImage} alt="유저가 선택한 이미지" fill />}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button className={classes.button} type="button" onClick={handlePickClick}>
+          이미지 선택
+        </button>
+      </div>
+    </div>
+  );
+}
