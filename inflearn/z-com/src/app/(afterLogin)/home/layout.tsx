cb0b6c0e97410f50dbf46ff5d69539711956cc59
김동민 <anwjr41@naver.com>
2024-08-06 01:50:30 +0900

    1챕터 중간 저장

--- inflearn/z-com/src/app/(afterLogin)/home/layout.tsx
+++ inflearn/z-com/src/app/(afterLogin)/home/layout.tsx
@@ -1,3 +1,12 @@
-export default async function HomeLayout({ children }) {
-  return <div>홈 레이아웃{children}</div>;
+import { ReactNode } from "react";
+
+type Props = { children: ReactNode };
+
+export default async function HomeLayout({ children }: Props) {
+  return (
+    <div>
+      홈 레이아웃
+      {children}
+    </div>
+  );
 }
