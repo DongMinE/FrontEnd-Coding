75e51d3bbcd0f588657ac845982fac1bffb88172
김동민 <anwjr41@naver.com>
2024-08-23 17:35:36 +0900

    캘린더 스크롤 안되는 1주일치 원본

--- lunchlab/src/app/(afterLogin)/oderList/_component/CustumCalendarTest.tsx
+++ lunchlab/src/app/(afterLogin)/oderList/_component/CustumCalendarTest.tsx
@@ -2,25 +2,23 @@
 
 import React, { useState } from "react";
 import { format, startOfWeek, addDays, addWeeks, subWeeks } from "date-fns";
-import style from "./CustumCalendar.module.css";
+import style from "./CustumCalendarTest.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 export default function CustumCalendar() {
   const [currentWeek, setCurrentWeek] = useState(new Date());
-
   const startOfCurrentWeek = startOfWeek(currentWeek, { weekStartsOn: 0 }); // 일요일 시작
-
-  // 날짜 배열 생성
   const days = Array.from({ length: 7 }).map((_, i) => format(addDays(startOfCurrentWeek, i), "d"));
-
-  // 요일 배열 생성
   const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
-  // 다음 주로 이동
   const nextWeek = () => {
     setCurrentWeek(addWeeks(currentWeek, 1));
   };
 
-  // 이전 주로 이동
   const prevWeek = () => {
     setCurrentWeek(subWeeks(currentWeek, 1));
   };
@@ -30,33 +28,26 @@ export default function CustumCalendar() {
       <div className={style.calendarBox}>
         <div>주문내역</div>
         <div className={style.monthDiv}>
-          {/* 월 */}
+          <button onClick={prevWeek}>{"<"}</button>
           <span className={style.monthSpan}>{format(currentWeek, "M")}월</span>
+          <button onClick={nextWeek}>{">"}</button>
         </div>
-        <button onClick={prevWeek} style={{ marginRight: "10px" }}>
-          {"<"}
-        </button>
-        <div>
+        <div className={style.dateInfo}>
           <div className={style.weekDiv}>
-            {/* 요일 */}
             {daysOfWeek.map((day, index) => (
-              <span className={style.weekSpan} key={index}>
+              <div className={style.weekSpan} key={index}>
                 {index + 1 === parseInt(format(new Date(), "e")) ? <b>오늘</b> : day}
-              </span>
+              </div>
             ))}
           </div>
           <div className={style.dayDiv}>
-            {/* 날짜 */}
             {days.map((day, index) => (
-              <span className={style.daySpan} key={index}>
+              <div className={style.daySpan} key={index}>
                 {day}
-              </span>
+              </div>
             ))}
           </div>
         </div>
-        <button onClick={nextWeek} style={{ marginLeft: "10px" }}>
-          {">"}
-        </button>
       </div>
     </section>
   );
